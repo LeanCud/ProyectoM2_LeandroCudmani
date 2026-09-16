4d6cda63fb5d8ed5c457a368fc6622d3f15e075d
LeanCud <leandro.cudmani.tucu@gmail.com>
2026-09-16 18:08:38 -0300

    Creando nuevas rutas funcionales para authors

--- src/controllers/authorsControllers.js
+++ src/controllers/authorsControllers.js
@@ -53,3 +53,32 @@ export const createAuthor = async (req, res) => {
     res.status(500).json({error: "Error creando Autor"});
   }
 };
+
+export const updateAuthor = async (req, res) => {
+  try {
+      const { id } = req.params
+  const { name, email, bio } = req.body
+
+  if (!name || ! email) {
+    return res.status(400).json({error : "El Nombre y el Email son requeridos"});
+  }
+
+  const result = await pool.query(`UPDATE authors SET name = $1, email = $2, bio = $3 WHERE id = $4 RETURNING *`, [name, email, bio || null, id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({error: "Autor no encontrado",});
+  }
+
+  res.status(200).json(result.rows[0]);
+
+  } catch (error) {
+    console.error("Error Actualizando Autor", error);
+
+    if (error.code === "23505") {
+      return res.status(409).json({error: "El Email ya fue registrado"});
+    }
+
+    res.status(500).json({error: "Error Actualizando Autor"});
+  }
+};
+
